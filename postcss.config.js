a74ff67cc03616b27f90d731d556debe604290b9
blaytenshi <blay.tenshi@gmail.com>
2020-03-30 22:46:17 +1100

    Init Commit
    Tailwind Integrated

---
+++ postcss.config.js
@@ -0,0 +1,20 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+  // Specify the paths to all of the template files in your project 
+  content: [
+    './src/**/*.js',
+    './public/index.html',
+  ],
+
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+});
+
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    ...process.env.NODE_ENV === 'production'
+      ? [purgecss]
+      : []
+  ]
+};
