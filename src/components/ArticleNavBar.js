381be387c8a0c0246e49d05e9891609232267288
Jimmy Tang <blay.tenshi@gmail.com>
2020-04-11 21:21:34 +1000

    Article Nav
    Tidied up ArticleNav
    Added Back Button

--- src/components/ArticleNavBar.js
+++ src/components/ArticleNavBar.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
+import { ReactComponent as Back } from "../assets/img/back.svg"
 
 const ArticleNavBar = () => {
   
   let history = useHistory();
   
   return (
-    <div>
-      <a className="text-red-500 underline" onClick={() => history.goBack()}>Back</a>
+    <div className="px-8">
+      <button className="text-red-500 underline text-xl flex items-center" onClick={() => history.goBack()}>
+        <Back className="h-6" />
+        <span className="pl-2">Back</span>
+      </button>
     </div>
   )
 };
