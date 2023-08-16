3bbb70b177af70eca8d606985242731c5aa82697
Jimmy Tang <blay.tenshi@gmail.com>
2023-08-16 21:58:02 +1000

    Refactored pages.json to only render widgets!

--- src/widgets/index.js
+++ src/widgets/index.js
@@ -1,22 +1,30 @@
-import JourneyFromPaperToDigital from './journey-from-paper-to-digital.jsx';
 import Footer from './footer.jsx';
 import HeroBanner from './hero-banner.jsx';
 import Home from './home.jsx';
+import JourneyFromPaperToDigital from './journey-from-paper-to-digital.jsx';
+import MenuBar from './menu-bar.jsx';
+import MyLearning from './my-learning.jsx';
+import MyWork from './my-work.jsx';
 import NoMatch from './no-match.jsx';
 import OriginStory from './origin-story.jsx';
 import PersonalWebsite from './personal-website.jsx';
+import SelfIntroduction from './self-introduction.jsx';
 import ThisMyMob from './this-my-mob.jsx';
 import Tina from './tina.jsx';
 import Tkssir from './tkssir.jsx';
 
 export default {
-  JourneyFromPaperToDigital,
   Footer,
   HeroBanner,
   Home,
+  JourneyFromPaperToDigital,
+  MenuBar,
+  MyLearning,
+  MyWork,
   NoMatch,
   OriginStory,
   PersonalWebsite,
+  SelfIntroduction,
   ThisMyMob,
   Tina,
   Tkssir,
